113a58388abd066b8b3e182c5bd1524f84f43a4a
4okoboi <zakiroffkam@gmail.com>
2024-09-15 18:30:32 +0300

    updated main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from app.warehouse.router import warehouse_router
 from app.auth.handlers import user_router
 from app.auth.login_handler import login_router
+from app.application.router import application_router
 
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,6 +27,7 @@ main_router = APIRouter()
 main_router.include_router(warehouse_router, prefix="/warehouse", tags=["warehouse"])
 main_router.include_router(user_router, prefix="/user", tags=["user"])
 main_router.include_router(login_router, prefix="/login", tags=["login"])
+main_router.include_router(application_router, prefix="/application", tags=["application"])
 
 app.include_router(main_router)
 
