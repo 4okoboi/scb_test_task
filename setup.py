09bb2c96b4c1021abe4017ce115f741795c67993
4okoboi <zakiroffkam@gmail.com>
2024-09-16 04:01:23 +0300

    checking

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 import requests
 import time
 
-time.sleep(10)  # Задержка в 10 секунд
+def make_requests():
+    time.sleep(10)  # Задержка в 10 секунд
 
-payload_admin = {
-        "username": "test_admin",
-        "email": "admin@test.ru",
-        "password": "test"
+    payload_admin = {
+            "username": "test_admin",
+            "email": "admin@test.ru",
+            "password": "test"
+        }
+
+
+    requests.post(
+        url="http://localhost:8000/user/admin",
+        json=payload_admin
+    )
+
+    payload_client = {
+        "username":  'ООО "ООО"',
+        "password": "client",
+        "email": "client@test.ru",
+        "actual_address": "Казань, Аметьево, 20"
     }
 
+    requests.post(
+        url="http://localhost:8000/user/client",
+        json=payload_client
+    )
 
-requests.post(
-    url="http://localhost:8000/user/admin",
-    json=payload_admin
-)
-
-payload_client = {
-    "username":  'ООО "ООО"',
-    "password": "client",
-    "email": "client@test.ru",
-    "actual_address": "Казань, Аметьево, 20"
-}
-
-requests.post(
-    url="http://localhost:8000/user/client",
-    json=payload_client
-)
-
-exit(code=20)
+if __name__ == "__main__":
+    make_requests()
