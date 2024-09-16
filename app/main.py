0d8af1acebc9265fc30142ebe03b2a7b2f4e9cd6
4okoboi <zakiroffkam@gmail.com>
2024-09-16 04:14:43 +0300

    final commit

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from app.warehouse.router import warehouse_router
 from app.auth.handlers import user_router
 from app.auth.login_handler import login_router
 from app.application.router import application_router
-from app.initializer import Initializer
-
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -31,7 +29,6 @@ main_router.include_router(user_router, prefix="/user", tags=["user"])
 main_router.include_router(login_router, prefix="/login", tags=["login"])
 main_router.include_router(application_router, prefix="/application", tags=["application"])
 app.include_router(main_router)
-Initializer()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
